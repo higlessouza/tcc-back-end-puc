ded6a30104c28451d7125b22d59b90152b5b897a
Higles Souza <43656269+higlessouza@users.noreply.github.com>
2021-04-09 21:53:20 -0300

    Comit Inicial

---
+++ tcc-back-end-puc/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace tcc_back_end_puc
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
