7a65502b24d0b2a0ecea859646e8d7c4cba1f8d3
Higles Souza <higles.souza@localiza.com>
2021-05-24 14:00:36 -0300

    Alteração e-mail de recuperação de senha

--- tcc-back-end-puc/Infrastructure/Repositories/EmailRepository.cs
+++ tcc-back-end-puc/Infrastructure/Repositories/EmailRepository.cs
@@ -29,11 +29,37 @@ namespace tcc_back_end_puc.Infrastructure.Repositories
                 //.Bcc.Add(new System.Net.Mail.MailAddress(textBoxCCo.Text));
                 //.CC.Add(new System.Net.Mail.MailAddress("5544"));
                 Subject = "Reuperação de senha - TCC Puc Minas",
-                Body = $"{codigoRecuperacao}"
+                IsBodyHtml = true,
+                Body = ObterCorpoEmail(codigoRecuperacao)
             };
             mail.To.Add(new MailAddress(destinatario));
             smtp.Send(mail);
             return codigoRecuperacao;
+        }
+
+        public string ObterCorpoEmail( int codigo)
+        {
+            return @" <!DOCTYPE html>
+                    <html>
+                    <head>
+                    <style>
+                    body {background-color: white;} h1 {color: black; text-align: left;}                  
+                    p {font-family: verdana;font-size:20px;}                    
+                    img {display: block; margin-left: auto; margin-right: auto}
+                    </style>
+                    </head>
+                    <body>
+                    <img src='https://ipuc.pucminas.br/green/documentos/green/logo_pucminas.png' width = '130px'/>
+                    <h1> Redefinição de senha</h1>
+                    <p> Olá, 
+                    Recebemos uma solicitação para restaurar sua senha de acesso em nosso site.  
+                    Por favor, utilize o código <b>"+codigo+@"</b> para concluir a redefinição.</p>
+                    <p> Atenciosamente, </p>
+                    <p> TCC Puc Minas, </p>
+                    <img src ='https://uxyja.stripocdn.email/content/guids/CABINET_1ce849b9d6fc2f13978e163ad3c663df/images/3991592481152831.png' width = '100%'/>
+                     </body>
+                     </html>
+          ";
         }
     }
 }
